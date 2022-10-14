c1127a11ac86fdd2d61533d85ff99c3bacdddb37
Pavlo Supenko <pavlo.supenko@bini.games>
2022-10-14 13:27:42 +0300

    Read tests order from file;

--- main.py
+++ main.py
@@ -13,22 +13,32 @@ print(f"platform.system: {platform.system()}")
 print(f"platform.release: {platform.release()}")
 
 androidTestsRunner.run_appium_session("", "", "")
-tests = [
-    "system_dialogs_skip_test.py",
-    "privacy_policy_skip_test.py",
-    "questionnaire_skip_test.py",
-    "purchase_menu_open_test.py"
-]
+testsOrderFile = open("tests.txt", "r")
+testNames = testsOrderFile.read().split('\n')
 
-for testName in tests:
-    enterArgsString = f"tests/examples/{testName} -k enter -s --junit-xml $DEVICEFARM_LOG_DIR/{testName}.enter.xml"
+print('Test names found:')
+testNumber = 1
+for testName in testNames:
+    print(f"{testNumber}.{testName}")
+    testNumber += 1
+
+
+generalArtifactsDirectory = os.environ['DEVICEFARM_LOG_DIR']
+testLogsDirectory = os.path.join(generalArtifactsDirectory, "Logs")
+
+for testName in testNames:
+    currentTestLogDirectory = os.path.join(testLogsDirectory, testName)
+    currentEnterTestLogPath = os.path.join(currentTestLogDirectory, "enter.xml")
+    currentExitTestLogPath = os.path.join(currentTestLogDirectory, "exit.xml")
+
+    enterArgsString = f"tests/{testName}.py -k enter -s --junit-xml {currentEnterTestLogPath}"
     enterArgs = enterArgsString.split(" ")
     enterRetCode = pytest.main(enterArgs)
-    print(f"Ret code for: {testName}.enter is: {enterRetCode}")
+    print(f"Return code for: {testName}.enter is: {enterRetCode}")
 
     time.sleep(2)
 
-    exitArgsString = f"tests/examples/{testName} -k exit -s --junit-xml $DEVICEFARM_LOG_DIR/{testName}.exit.xml"
+    exitArgsString = f"tests/{testName}.py -k exit -s --junit-xml {currentExitTestLogPath}"
     exitArgs = exitArgsString.split(" ")
     exitRetCode = pytest.main(exitArgs)
-    print(f"Ret code for: {testName}.exit is: {exitRetCode}")
+    print(f"Return code for: {testName}.exit is: {exitRetCode}")
